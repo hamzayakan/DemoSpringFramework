e4fcb68b6e879a156e8162f51164649f3663ec2d
hamzayakan <68115336+hamzayakan@users.noreply.github.com>
2022-07-01 15:44:46 +0300

    commit

--- src/main/java/com/hamzayakan/bean/JsfBean.java
+++ src/main/java/com/hamzayakan/bean/JsfBean.java
@@ -1,22 +1,20 @@
 package com.hamzayakan.bean;
 
+
 import lombok.Getter;
 import lombok.Setter;
-
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named(value = "cdiBean")
 @RequestScoped
-@Setter @Getter
+@Getter @Setter
 public class JsfBean {
 
     private String patika;
 
     public JsfBean() {
-
-        patika="Spring Boot Eğitimine Hoşgeldiniz";
+        patika="Spring Boot Eğitime Hoşgeldiniz";
     }
-
 }
